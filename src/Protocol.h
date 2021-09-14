9257434cf695a490d8141ef335bfda6378279566
Vasilis Tsaknis <tsaknis_vasilios@hotmail.com>
2021-09-14 17:10:42 +0300

    Moving Protocol Struct info protocol.h file

--- src/Protocol.h
+++ src/Protocol.h
@@ -24,10 +24,52 @@
  * These are combined to form Tri-State bits when sending or receiving codes.
  */
 
+/**
+ * Description of a single pule, which consists of a high signal
+ * whose duration is "high" times the base pulse length, followed
+ * by a low signal lasting "low" times the base pulse length.
+ * Thus, the pulse overall lasts (high+low)*pulseLength
+ */
+struct HighLow {
+    uint8_t high;
+    uint8_t low;
+};
+
+/**
+ * A "protocol" describes how zero and one bits are encoded into high/low
+ * pulses.
+ */
+struct Protocol {
+    /** base pulse length in microseconds, e.g. 350 */
+    uint16_t pulseLength;
+
+    HighLow syncFactor;
+    HighLow zero;
+    HighLow one;
+
+    /**
+     * If true, interchange high and low logic levels in all transmissions.
+     *
+     * By default, RCSwitch assumes that any signals it sends or receives
+     * can be broken down into pulses which start with a high signal level,
+     * followed by a a low signal level. This is e.g. the case for the
+     * popular PT 2260 encoder chip, and thus many switches out there.
+     *
+     * But some devices do it the other way around, and start with a low
+     * signal level, followed by a high signal level, e.g. the HT6P20B. To
+     * accommodate this, one can set invertedSignal to true, which causes
+     * RCSwitch to change how it interprets any HighLow struct FOO: It will
+     * then assume transmissions start with a low signal lasting
+     * FOO.high*pulseLength microseconds, followed by a high signal lasting
+     * FOO.low*pulseLength microseconds.
+     */
+    bool invertedSignal;
+};
+
 #if defined(ESP8266) || defined(ESP32)
-    static const VAR_ISR_ATTR RCSwitch::Protocol proto[] = {
+    static const VAR_ISR_ATTR Protocol proto[] = {
 #else
-    static const RCSwitch::Protocol PROGMEM proto[] = {
+    static const Protocol PROGMEM proto[] = {
 #endif
     { 350, {  1, 31 }, {  1,  3 }, {  3,  1 }, false },    // protocol 1
     { 650, {  1, 10 }, {  1,  2 }, {  2,  1 }, false },    // protocol 2
